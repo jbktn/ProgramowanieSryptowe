fabec118ca4ecd639d206ac93af13c36e6b552d0
Jakub Pomorski <delveller1@gmail.com>
2023-10-18 00:46:57 +0200

    lab2

--- lab2/skrypt3.py
+++ lab2/skrypt3.py
@@ -7,20 +7,20 @@ import sys
 def main():
     parser = argparse.ArgumentParser(description="skrypt3")
     parser.add_argument('command', type=str, help='grep, cut lub skrypt1')
-    parser.add_argument('-d', dest='delimiter')
-    parser.add_argument('-f', dest='field_number', type=int)
-    parser.add_argument('-i', dest='ignore_case', action='store_true')
-    parser.add_argument('-w', dest='whole_word', action='store_true')
+    parser.add_argument('-d', dest='delimit')
+    parser.add_argument('-f', dest='number', type=int)
+    parser.add_argument('-i', dest='case', action='store_true')
+    parser.add_argument('-w', dest='word', action='store_true')
     parser.add_argument('pattern', type=str)
 
     args = parser.parse_args()
 
     if args.command == 'cut':
         input_lines = sys.stdin.read()
-        result = cut(input_lines, args.delimiter, args.field_number)
+        result = cut(input_lines, args.delimit, args.number)
     elif args.command == 'grep':
         input_lines = sys.stdin.read()
-        result = grep(args.pattern, input_lines, args.ignore_case, args.whole_word)
+        result = grep(args.pattern, input_lines, args.case, args.word)
     else:
         ops(args.command)
         
